a561c6ed56ff5445cf5eb9a1a7635a0b6f36185d
Zulfikar Ramadhan <ramadhan.fiki2@gmail.com>
2022-05-17 15:55:35 +0700

    membuat layout login page aplikasi

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.example.myapplication
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
+import androidx.annotation.RequiresApi
 
 class MainActivity : AppCompatActivity() {
+  @RequiresApi(Build.VERSION_CODES.R)
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
+    supportActionBar?.hide()
     setContentView(R.layout.activity_main)
   }
 }
