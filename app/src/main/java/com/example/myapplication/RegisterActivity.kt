8446998f79a021dda79c60336034358291d5b79e
Zulfikar Ramadhan <ramadhan.fiki2@gmail.com>
2022-05-25 10:29:17 +0700

    tambah activity register layout

--- app/src/main/java/com/example/myapplication/RegisterActivity.kt
+++ app/src/main/java/com/example/myapplication/RegisterActivity.kt
@@ -3,10 +3,10 @@ package com.example.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class SpinnerActivity : AppCompatActivity() {
+class RegisterActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_spinner)
+    setContentView(R.layout.activity_register)
     supportActionBar?.hide()
   }
 }
