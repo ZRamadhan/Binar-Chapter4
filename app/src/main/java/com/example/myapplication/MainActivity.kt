eb6998d7f02fa65a67735ab3777af0c0e43b6ce1
Hendika N <hendika.nugroho@loratechai.com>
2022-05-25 12:03:50 +0700

    tambah navigasi halaman menggunakan intent

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,16 +1,25 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.annotation.RequiresApi
 
 class MainActivity : AppCompatActivity() {
   @RequiresApi(Build.VERSION_CODES.R)
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    supportActionBar?.hide()
     setContentView(R.layout.activity_main)
+    supportActionBar?.hide()
+
+    val btnSignUp = findViewById<TextView>(R.id.btnNavigateToSignup)
+
+    btnSignUp.setOnClickListener{
+      val navigateSignUp = Intent(this@MainActivity, RegisterActivity::class.java)
+      startActivity(navigateSignUp)
+    }
   }
 }
--- app/src/main/java/com/example/myapplication/RegisterActivity.kt
+++ app/src/main/java/com/example/myapplication/RegisterActivity.kt
@@ -1,12 +1,21 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class RegisterActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_register)
     supportActionBar?.hide()
+
+    val btnLogin = findViewById<TextView>(R.id.btnNavigateToSignin)
+
+    btnLogin.setOnClickListener{
+      val navigateSignIn = Intent(this@RegisterActivity, MainActivity::class.java)
+      startActivity(navigateSignIn)
+    }
   }
 }
